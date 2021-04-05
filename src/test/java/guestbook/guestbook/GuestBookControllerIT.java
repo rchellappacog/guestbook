63b96a4823bcb394eea04fb85c0b2e095e35d787
rchellappacog <Ravindran.Chellappa@cognizant.com>
2021-04-05 19:30:06 -0400

    Fail Test 1

---
+++ src/test/java/guestbook/guestbook/GuestBookControllerIT.java
@@ -0,0 +1,31 @@
+package guestbook.guestbook;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+
+public class GuestBookControllerIT {
+@Autowired
+    MockMvc mockMvc;
+@Autowired
+    ObjectMapper objectMapper;
+@Test
+void create_FetchAll() throws Exception {
+    mockMvc.perform(
+            post("/guestbook")
+            .content(objectMapper.writeValueAsString("Hello world!"))
+            .contentType(MediaType.APPLICATION_JSON)
+    ).andExpect(status().isCreated());
+}
+}
