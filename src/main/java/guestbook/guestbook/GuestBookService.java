eefe648221bd956bebe10833ec6f24b7a485260c
sebin <sebin.francis@cognizant.com>
2021-04-06 18:49:58 -0400

    added fetchall to guestbook service

--- src/main/java/guestbook/guestbook/GuestBookService.java
+++ src/main/java/guestbook/guestbook/GuestBookService.java
@@ -2,6 +2,9 @@ package guestbook.guestbook;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class GuestBookService {
     private final GuestBookRepository guestBookRepository;
@@ -15,5 +18,11 @@ public class GuestBookService {
         this.guestBookRepository.save(
                 new GuestBookEntity(guestBookDto.getName(), guestBookDto.getComment())
         );
+    }
+
+    public List<GuestBookDto> fetchAll() {
+        return this.guestBookRepository.findAll().stream().map(
+                guestBookEntity -> new GuestBookDto(guestBookEntity.getName(), guestBookEntity.getComment())
+        ).collect(Collectors.toList());
     }
 }
--- src/test/java/guestbook/guestbook/GuestBookServiceTest.java
+++ src/test/java/guestbook/guestbook/GuestBookServiceTest.java
@@ -6,7 +6,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class GuestBookServiceTest {
@@ -24,6 +28,24 @@ public class GuestBookServiceTest {
 
         verify(mockGuestBookRepository).save(
                 new GuestBookEntity("peter", "it is a nice place to visit!")
+        );
+    }
+
+    @Test
+    void fetchAllTest() {
+        GuestBookEntity entity1 = new GuestBookEntity("peter", "it is a nice place to visit!");
+        GuestBookEntity entity2 = new GuestBookEntity("john", "nice artifacts!");
+
+        when(mockGuestBookRepository.findAll()).thenReturn(
+             List.of(entity1, entity2)
+        );
+
+        List<GuestBookDto> actual = subject.fetchAll();
+        assertThat(actual).isEqualTo(
+                List.of(
+                        new GuestBookDto("peter", "it is a nice place to visit!"),
+                        new GuestBookDto("john", "nice artifacts!")
+                )
         );
     }
 }
