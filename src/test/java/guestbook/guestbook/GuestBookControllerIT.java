c3394b67b98d2f9377efe6317f9e7f5e02825723
sebin <sebin.francis@cognizant.com>
2021-04-08 18:16:29 -0400

    local profile for test

--- src/test/java/guestbook/guestbook/GuestBookControllerIT.java
+++ src/test/java/guestbook/guestbook/GuestBookControllerIT.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -16,6 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("local")
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class GuestBookControllerIT {
     @Autowired
