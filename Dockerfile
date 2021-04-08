32a6a92c8fa3447f5b2aac4cd85ec19c4d05d91d
sebin <sebin.francis@cognizant.com>
2021-04-07 20:08:50 -0400

    working heroku app

--- Dockerfile
+++ Dockerfile
@@ -10,5 +10,6 @@ RUN mvn -f /home/app/pom.xml clean package
 #
 FROM openjdk:11-jre-slim
 COPY --from=build /home/app/target/guestbook-0.0.1-SNAPSHOT.jar /usr/local/lib/demo.jar
-EXPOSE 8080
-ENTRYPOINT ["java","-jar","/usr/local/lib/demo.jar"]
+#EXPOSE 8080
+#ENTRYPOINT ["java","-jar","/usr/local/lib/demo.jar"]
+CMD [ "sh", "-c", "java -Xmx300m -Xss512k -Dserver.port=$PORT -jar /usr/local/lib/demo.jar" ]
--- src/main/java/guestbook/guestbook/GuestbookApplication.java
+++ src/main/java/guestbook/guestbook/GuestbookApplication.java
@@ -18,23 +18,4 @@ public class GuestbookApplication {
 		SpringApplication.run(GuestbookApplication.class, args);
 	}
 
-    @Configuration
-    @Profile("heroku")
-    public static class DatabaseConfig {
-
-        @Bean
-        public DataSource dataSource() throws URISyntaxException {
-            URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
-            String username = dbUri.getUserInfo().split(":")[0];
-            String password = dbUri.getUserInfo().split(":")[1];
-            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-
-            return DataSourceBuilder.create()
-                    .url(dbUrl)
-                    .username(username)
-                    .password(password)
-                    .build();
-        }
-    }
 }
